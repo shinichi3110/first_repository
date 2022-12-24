516e1d7415e0fb688574f2a3d8052c9776003224
shinichi3110 <121342818+shinichi3110@users.noreply.github.com>
2022-12-24 21:08:38 +0900

    initial commits

---
+++ 複数演算/full.js
@@ -0,0 +1,11 @@
+function submit() {
+    num01 = document.getElementById('num01');
+    num02 = document.getElementById('num02');
+    num03 = document.getElementById('num03');
+
+    a = num01.value;
+    b = num02.value;
+    c = num03.value;
+
+    alert(a*b/c);
+}
